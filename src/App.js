d0a9c5c64c63135f3018737ee51898008c2273ea
Isabel Abonitalla <isabelabonitalla@gmail.com>
2022-09-24 05:21:35 -0400

    map changes, currently nyc but no marker

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component, useMemo } from "react";
 import './App.css';
 import Mapdata from "./server/2022_JULY.json";
 import Data from './components/data';
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 // const GOOGLE_MAP_API = process.env.GOOGLE_MAP_API;
 // console.log("API KEY: " + GOOGLE_MAP_API);
 // <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5qz7stbp7962yxHp_Udjp0rf_ZuIP2a0&callback=initMap">
 // </script>
+
+const containerStyle = {
+  width: '800px',
+  height: '800px'
+};
+
+const center = {
+  lat: 40.7412012,
+  lng: -73.9890337
+};
+
+const mark = {
+  lat: 40.7300939,
+  lng: -73.9908372
+}
 
 class App extends Component{
   constructor(props) {
@@ -22,6 +37,21 @@ class App extends Component{
     return(
       <div className="app">
         <div>hello sanity check</div>
+        <LoadScript
+          googleMapsApiKey="AIzaSyA5qz7stbp7962yxHp_Udjp0rf_ZuIP2a0"
+        >
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <Marker
+              icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}
+              position={mark}
+            />
+          </GoogleMap>
+        </LoadScript>
         <Data Mapdata={Mapdata} />
       </div>
       );
