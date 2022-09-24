25833f21c34ba3b49506a7369583fc6e1bfa4e7f
Isabel Abonitalla <isabelabonitalla@gmail.com>
2022-09-24 04:09:40 -0400

    working console logs

--- src/components/data.js
+++ src/components/data.js
@@ -1,27 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCFUzm27n9IkIV820A_VyyvCXb9v8cBI8A",
-  authDomain: "hackher-9b4ed.firebaseapp.com",
-  databaseURL: "https://hackher-9b4ed.firebaseio.com",
-  projectId: "hackher-9b4ed",
-  storageBucket: "hackher-9b4ed.appspot.com",
-  messagingSenderId: "875167518655",
-  appId: "1:875167518655:web:16daa8339defad35601b0a"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCFUzm27n9IkIV820A_VyyvCXb9v8cBI8A",
+//   authDomain: "hackher-9b4ed.firebaseapp.com",
+//   databaseURL: "https://hackher-9b4ed.firebaseio.com",
+//   projectId: "hackher-9b4ed",
+//   storageBucket: "hackher-9b4ed.appspot.com",
+//   messagingSenderId: "875167518655",
+//   appId: "1:875167518655:web:16daa8339defad35601b0a"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 import React from "react";
 import PropTypes from 'prop-types';
-import '../css/data.css';
+// import '../css/data.css';
 
 const DataDisplay = ({Mapdata}) => Mapdata.map((timelineObjects, index) => {
 
@@ -30,7 +28,7 @@ const DataDisplay = ({Mapdata}) => Mapdata.map((timelineObjects, index) => {
     // console.log(typeof(pins));
     if(typeof(pins) == "object"){
         // console.log(pins);
-        console.log(pins.location.address)
+        console.log(pins.location.address, pins.location.latitudeE7, pins.location.longitudeE7)
     }
     // for(const item in pins) {
     //     if(typeof(item) != "undefined"){
